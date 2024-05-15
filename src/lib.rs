650e05cc96a97717f347f2a4146f7dc77ed106ee
Kaka <hailongma61@gmail.com>
2024-05-15 18:45:34 +0800

    feature: use darling to process EnumFrom macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,49 +1,23 @@
-// proc macro crate
+mod enum_from;
+mod enum_from_darling;
 
+use enum_from::process_enum_from;
+use enum_from_darling::process_enum_from_darling;
 use proc_macro::TokenStream;
-use quote::quote;
 
-// for enum, we'd like to generate From impls for each variant
+/// for enum, we'd like to generate From impls for each variant
 #[proc_macro_derive(EnumFrom)]
 pub fn derive_enum_from(input: TokenStream) -> TokenStream {
     let input = syn::parse_macro_input!(input as syn::DeriveInput);
     // println!("{:#?}", input);
 
-    // get the ident
-    let ident = input.ident;
+    process_enum_from(input).into()
+}
 
-    // get enum variants
-    let variants = match input.data {
-        syn::Data::Enum(data) => data.variants,
-        _ => panic!("EnumFrom only works on enums"),
-    };
+#[proc_macro_derive(EnumFromDarling)]
+pub fn derive_enum_from_darling(input: TokenStream) -> TokenStream {
+    let input = syn::parse_macro_input!(input as syn::DeriveInput);
+    // println!("{:#?}", input);
 
-    // for each variant, get the ident and fields
-    let from_impls = variants.iter().map(|variant| {
-        let var = &variant.ident;
-        match &variant.fields {
-            syn::Fields::Unnamed(fields) => {
-                if fields.unnamed.len() != 1 {
-                    quote! {}
-                } else {
-                    let field = fields.unnamed.first().expect("should have 1 field");
-                    let ty = &field.ty;
-                    quote! {
-                        impl From<#ty> for #ident {
-                            fn from(v: #ty) -> Self {
-                                #ident::#var(v)
-                            }
-                        }
-                    }
-                }
-            }
-            syn::Fields::Unit => quote! {},
-            syn::Fields::Named(_fields) => quote! {},
-        }
-    });
-
-    quote! {
-        #( #from_impls )*
-    }
-    .into()
+    process_enum_from_darling(input).into()
 }
